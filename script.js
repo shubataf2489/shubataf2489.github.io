957d2a3ff0740b6a2e2a40df3337c5ef1cb4fc85
shubata <shubataf@nycstudents.net>
2025-01-27 22:44:13 -0500

    done

--- script.js
+++
@@ -1,9 +0,0 @@
-let ownerName = "Shubata"; // update the part BETWEEN the "quotes"
-let userName = " shubataf2489"; // same here
-
-document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
-});
-
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
